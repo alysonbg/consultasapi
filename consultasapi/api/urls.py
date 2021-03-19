a3870128440ed7ef59d3b02bd711e915aff7b7d3
Alyson Girao <alysongirao@gmail.com>
2021-03-19 16:47:55 -0300

    Adicionada view para listar as agendas

--- consultasapi/api/urls.py
+++ consultasapi/api/urls.py
@@ -5,4 +5,6 @@ from consultasapi.api import views
 
 urlpatterns = [
     path('especialidades/', views.ListEspecialidades.as_view(), name='especialidades'),
+    path('medicos/', views.ListMedicos.as_view(), name='medicos'),
+    path('agendas/', views.ListAgendas.as_view(), name='agendas'),
 ]
--- consultasapi/api/views.py
+++ consultasapi/api/views.py
@@ -9,3 +9,38 @@ class ListEspecialidades(generics.ListAPIView):
     serializer_class = serializers.EspecialidadeSerializer
     permission_classes = [IsAuthenticated]
     queryset = models.Especialidade.objects.all()
+
+
+class ListMedicos(generics.ListAPIView):
+    search_fields = ['nome']
+    filter_backends = (filters.SearchFilter, )
+    serializer_class = serializers.MedicoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = models.Medico.objects.all()
+        especialidades = self.request.GET.getlist('especialidade', None)
+        if especialidades:
+            return queryset.filter(especialidade__in=especialidades)
+        return queryset
+
+
+class ListAgendas(generics.ListAPIView):
+    serializer_class = serializers.AgendaSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = models.Agenda.objects.all()
+        medicos = self.request.GET.getlist('medico', None)
+        especialidades = self.request.GET.getlist('especialidade', None)
+        data_inicio = self.request.query_params.get('data_inicio')
+        data_fim = self.request.query_params.get('data_fim')
+
+        if medicos:
+            queryset = queryset.filter(medico__in=medicos)
+        if especialidades:
+            queryset = queryset.filter(medico__especialidade__in=especialidades)
+        if data_inicio and data_fim:
+            queryset = queryset.filter(dia__range=(data_inicio, data_fim))
+
+        return queryset
